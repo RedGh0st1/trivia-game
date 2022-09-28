5a645f8ae71b73911335639879cd483f26e345af
Lennie Nurse <lnursej@gmail.com>
2022-09-28 11:52:48 -0400

    "update"

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const url = "https://opentdb.com/api.php?amount=10&type=multiple"
 
+// Getters
+const question = document.querySelector("#question")
+const answers = document.querySelector("#answer")
+const next = document.querySelector("#next")
+// console.log(question, answer, next)
+let updatedResults
+
+let index = 0
+
+const updateQuestion = () => {
+  for (let i = 0; i < 4; i++) {
+    const div = document.createElement("div")
+    const randomNum = Math.floor(
+      Math.random() * updatedResults[index].incorrect_answers.length
+    )
+    div.innerHTML = updatedResults[index].incorrect_answers.splice(randomNum, 1)
+    answers.append(div)
+  }
+}
+
 const getQuestions = async () => {
   //   fetch(`${url}`)
   //     .then((res) => res.json())
@@ -11,6 +31,19 @@ const getQuestions = async () => {
   const qs = await fetch(`${url}`)
   const qsJson = await qs.json()
   console.log(qsJson)
+
+  updatedResults = qsJson.results.map((quest) => {
+    quest.incorrect_answers.push(quest.correct_answer)
+    return quest
+  })
+  console.log(updatedResults)
+  question.innerHTML = updatedResults[index].question
 }
 
+next.addEventListener("click", () => {
+  index++
+  answers.innerHTML = ""
+  //   question.innerHTML = updatedResults[index].question
+  updateQuestion()
+})
 getQuestions()
